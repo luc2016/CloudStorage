137a4eef6a9c2f62e57b0e85e5ad3beb44e9dbc4
Lu Cui <lu.cui@loblaw.ca>
2018-06-18 15:23:31 -0400

    Initial Commit

---
+++ CloudStorage/CloudStorage.h
@@ -0,0 +1,19 @@
+//
+//  CloudStorage.h
+//  CloudStorage
+//
+//  Created by Lu Cui (LCL) on 2018-06-18.
+//  Copyright © 2018 lcl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CloudStorage.
+FOUNDATION_EXPORT double CloudStorageVersionNumber;
+
+//! Project version string for CloudStorage.
+FOUNDATION_EXPORT const unsigned char CloudStorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudStorage/PublicHeader.h>
+
+
